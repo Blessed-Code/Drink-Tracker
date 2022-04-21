709752fc875c9922e2e9bfa8d732d194b78eb5f9
YOGA <YOGA@DESKTOP-TQHRCDL>
2022-04-21 22:57:12 +0700

    kelar semua fitur

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
-let inputUsia = "Adult"; ///// ini buat INPUT usia !!!!!
+// let inputUsia = "Adult"; ///// ini buat INPUT usia !!!!!
 
 function totalPerhari(input) {
   let result = 0;
 
-  if (input === "Adult") {
-    result = 10000;
+  if (input === "Dewasa") {
+    result = 2000;
   } else {
-    result = 7500;
+    result = 1000;
   }
 
-  return result;
+  let targetMinum = document.getElementById("targetMinum");
+  targetMinum.innerHTML = result + " ml";
 }
+
+let indexUrl = document.URL.indexOf('=') + 1;
+let umur = document.URL.slice(indexUrl);
+console.log(umur);
+totalPerhari(umur);
 
 // console.log(totalPerhari(inputUsia));
 
-let totalAirPerHari = totalPerhari(inputUsia) /// ini untuk OUTPUT kebutuhan total per hari !!!!
+// let totalAirPerHari = totalPerhari(inputUsia) /// ini untuk OUTPUT kebutuhan total per hari !!!!
 
 
 //============================================
 
 
-let inputMinum = {jam:"11.00",minum: 1000}  // ini INPUT untuk minum ditambakah ke array !! INPUT minum = [jam, jumlah minum mililiter] !!!!
-// [ ["11.00", 1000],  ["13.00", 900], ["15.00", 900]]
+// let inputMinum = {jam:"11.00",minum: 1000}  // ini INPUT untuk minum ditambakah ke array !! INPUT minum = [jam, jumlah minum mililiter] !!!!
+// // [ ["11.00", 1000],  ["13.00", 900], ["15.00", 900]]
 
-function historyMinum(obj){
-    let result = []
+// function historyMinum(obj){
+//     let result = []
 
-    result.push(obj)
-    return result
-}
+//     result.push(obj)
+//     return result
+// }
 
-// console.log(historyMinum(inputMinum))
-let outputArrHistoryMinum = historyMinum(inputMinum) // ini untuk OUTPUT history minum !!!!
+// // console.log(historyMinum(inputMinum))
+// let outputArrHistoryMinum = historyMinum(inputMinum) // ini untuk OUTPUT history minum !!!!
 
 // {jam: 11:00
 //air: 1000}
@@ -42,34 +48,36 @@ let outputArrHistoryMinum = historyMinum(inputMinum) // ini untuk OUTPUT history
 // kurangnyahari ini = 10000-4500 \\ kebutuhal total == 10000
 
 
-function kurangHariIni(kebutuhan, arrHistory){
+// function kurangHariIni(kebutuhan, arrHistory){
 
-    let result = 0
+//     let result = 0
 
-    // console.log(kebutuhan,arrHistory)
-    let airHariIni = 0
+//     // console.log(kebutuhan,arrHistory)
+//     let airHariIni = 0
 
-    for(let i=0;i<arrHistory.length;i++){
-        airHariIni += arrHistory[i].minum
+//     for(let i=0;i<arrHistory.length;i++){
+//         airHariIni += arrHistory[i].minum
         
-    }
+//     }
 
-    result = kebutuhan - airHariIni
-    return result
+//     result = kebutuhan - airHariIni
+//     return result
 
-}
+// }
 
-let outputHistoryCoba = [{ jam: '13.00', minum: 900 }, { jam: '15.00', minum: 800 },{ jam: '17.00', minum: 1200 }]
+// let outputHistoryCoba = [{ jam: '13.00', minum: 900 }, { jam: '15.00', minum: 800 },{ jam: '17.00', minum: 1200 }]
 
-console.log(kurangHariIni(totalAirPerHari, outputHistoryCoba))
+// console.log(kurangHariIni(totalAirPerHari, outputHistoryCoba))
 // console.log(kurangHariIni(totalAirPerHari, outputArrHistoryMinum))
 
 // let outputKurangMinumHariIni = kurangHariIni(totalAirPerHari, outputArrHistoryMinum) /// ini untuk OUTPUT kekurangan air hari ini !!!!
 
 function tambah(angka){
     let progressMinum = document.getElementById("progressMinum");
-  
-    progressMinum.innerHTML = Number(progressMinum.innerHTML) + angka;
+    
+    const arrProgress = progressMinum.innerHTML.split(" ");
+
+    progressMinum.innerHTML = (Number(arrProgress[0]) + angka) + " ml";
   
     let historyTable = document.getElementById("historyTable");
     let newRowMinum = document.createElement("tr");
@@ -81,7 +89,8 @@ function tambah(angka){
     newButton.type = "submit";
     newButton.onclick = function () {
         let progressMinum = document.getElementById("progressMinum");
-        progressMinum.innerHTML = Number(progressMinum.innerHTML) - angka;
+        const arrProgress = progressMinum.innerHTML.split(" ");
+        progressMinum.innerHTML = (Number(arrProgress[0]) - angka) + " ml";
         newRowMinum.remove();
     };
 
